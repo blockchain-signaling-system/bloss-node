49340fa77082f44196df790d67f91e228f24536a
christiankiller <christian.killer@uzh.ch>
2018-10-31 13:38:58 +0100

    Wow, replace logger with console which apparently fixes the memory leak... :scream:

--- modules/sockets.js
+++ modules/sockets.js
@@ -1,14 +1,93 @@
 const logger = require('./logger');
 const ssh2shell = require('ssh2shell');
 
-module.exports = function (server) {
-    var io = require("socket.io").listen(server);
-    io.sockets.on('connection', function (socket) {
 
-        // Checks via simple ping -c1 if the configured controller is reachable 
-        // (so we won't even try to SSH if it's unreachable)
-        var isControllerAvailable = false; // change back to false
-        var statusPollingActive = false;
+// Function to retrieve the serviceName status on the controller configured in .env
+// function getServiceStatus(serviceName) {
+//     try {
+//         const systemCtlIsActive = require('./ssh/systemctl-is-active');
+//         command = ["sudo systemctl is-active " + serviceName];
+//         const sshExecutor = require('./sshexecutor')(server = systemCtlIsActive.server, commands = command, msg = systemCtlIsActive.msg, debug = systemCtlIsActive.debug, websocketChannel = 'statusChannel');
+//         var sshParamsForStatusRetrieval = sshExecutor.getSshExecutor();
+//         sshParamsForStatusRetrieval.onEnd = function (sessionTextt) {
+//             if (sessionTextt.includes("inactive")) {
+//                 logger.info(serviceName + " is inactive");
+//                 io.emit('statusChannel', { [serviceName]: "inactive" });
+//             } else if (sessionTextt.includes("active")) {
+//                 logger.info(serviceName + " is active");
+//                 io.emit('statusChannel', { [serviceName]: "active" });
+//             }
+//         };
+//         var SSH = new ssh2shell(sshParamsForStatusRetrieval);
+//         SSH.on('end', function (sessionTextt) {
+//             this.emit('msg', sessionTextt);
+//             logger.info('we here');
+//         })
+//         SSH.connect();
+
+
+//     } catch (error) {
+//         logger.error(error);
+//     }
+// }
+
+// // A function to execute bash commands (cmd)
+// // Second argument is used to immediatly trigger a service status update to the front end
+// function execSSH(cmd, service) {
+//     logger.info(cmd, service);
+//     try {
+//         const systemctlStatusBloss = require('./ssh/systemctl-status-bloss');
+//         const sshExecutor = require('./sshexecutor')(server = systemctlStatusBloss.server, systemctlStatusBloss.commands, systemctlStatusBloss.msg, systemctlStatusBloss.debug, 'statusChannel');
+//         var sshparams = sshExecutor.getSshExecutor();
+//         sshparams.commands = [cmd];
+//         sshparams.onEnd = function (sessionText, sshparams) {
+//             logger.info(JSON.stringify(sessionText));
+//             if (service != null) {
+//                 getServiceStatus(service);
+//             }
+
+//             console.log("sessionText is " + sessionText)
+//         };
+//         var SSH = new ssh2shell(sshparams);
+        
+//         SSH.connect();
+//     } catch (error) {
+//         logger.error(error);
+//     }
+// }
+
+
+
+module.exports = function (io) {
+    logger.info("Socket Handler invoked");
+
+    // Checks via simple ping -c1 if the configured controller is reachable 
+    // (so we won't even try to SSH if it's unreachable)
+    var isControllerAvailable = true; // change back to true
+    var statusPollingActive = false;
+
+    // This interval trigger a service status update every XX seconds
+    // TODO: Create Websocket and control to stop the interval / define thresholds
+    // clearInterval(timerID); // The setInterval it cleared and doesn't run anymore.
+    // setInterval(function () {
+    //     logger.info("Executing getServiceStatus Interval, isControllerAvailable:["+isControllerAvailable+"], statusPollingActive;["+statusPollingActive+"]");            
+    //     if (isControllerAvailable && statusPollingActive) {
+    //         //getServiceStatus("bloss");
+    //         //getServiceStatus("geth");
+    //         //getServiceStatus("ipfs");
+    //     }else {
+    //         if(!isControllerAvailable) {
+    //             logger.info("Status Retrieval failed because controller is not reachable");
+    //         } else if(!statusPollingActive) {
+    //             logger.info("Status Retrieval is deactivated");
+    //         } else {
+    //             logger.info("something else happened")
+    //         }
+    //     }
+    // }, 15 * 1000);
+
+    io.on('connection', function (socket) {
+        logger.info("connection established")
 
         // console.log("Executing child process");
         // var exec = require('child_process').exec, child;
@@ -23,9 +102,9 @@ module.exports = function (server) {
         //     }
         // });
 
-        socket.on('isControllerAvailable', function(data){
+        socket.on('isControllerAvailableRequest', function(data){
             logger.info("isControllerAvailable called");
-            socket.emit('isControllerAvailable', { "isControllerAvailable": isControllerAvailable });
+            //io.emit('isControllerAvailable', { "isControllerAvailable": isControllerAvailable });
         });
 
         socket.on('statusPolling', function(data){
@@ -38,95 +117,22 @@ module.exports = function (server) {
 
         // Endpoint for starting and stopping systemd services
         socket.on('serviceCtl', function (data) {
-            if (isControllerAvailable) {
-                switch (data.cmd) {
-                    case 'start':
-                        logger.info("Starting " + JSON.stringify(data.service));
-                        execSSH("sudo systemctl start " + data.service, data.service)
-                        break;
-                    case 'stop':
-                        logger.info("Stopping " + JSON.stringify(data.service));
-                        execSSH("sudo systemctl stop " + data.service, data.service)
-                        break;
-                    default:
-                        logger.error("Something went wrong with this request" + JSON.stringify(data));
-                }
+            logger.info("serviceCtl called");
+            // if (isControllerAvailable) {
+            //     switch (data.cmd) {
+            //         case 'start':
+            //             logger.info("Starting " + JSON.stringify(data.service));
+            //             execSSH("sudo systemctl start " + data.service, data.service)
+            //             break;
+            //         case 'stop':
+            //             logger.info("Stopping " + JSON.stringify(data.service));
+            //             execSSH("sudo systemctl stop " + data.service, data.service)
+            //             break;
+            //         default:
+            //             logger.error("Something went wrong with this request" + JSON.stringify(data));
+            //     }
 
-            }
-        });
-
-        // This interval trigger a service status update every XX seconds
-        // TODO: Create Websocket and control to stop the interval / define thresholds
-        // clearInterval(timerID); // The setInterval it cleared and doesn't run anymore.
-        setInterval(function () {
-            //logger.info("Executing getServiceStatus Interval, isControllerAvailable:["+isControllerAvailable+"], statusPollingActive;["+statusPollingActive+"]");            
-            if (isControllerAvailable && statusPollingActive) {
-                getServiceStatus("bloss");
-                getServiceStatus("geth");
-                getServiceStatus("ipfs");
-            }else {
-                if(!isControllerAvailable)
-                logger.info("Status Retrieval failed because controller is not reachable");
-                else if(!statusPollingActive)
-                logger.info("Status Retrieval is deactivated");
-            }
-        }, 15 * 1000);
-
-        // A function to execute bash commands (cmd)
-        // Second argument is used to immediatly trigger a service status update to the front end
-        function execSSH(cmd, service) {
-            logger.info(cmd, service);
-            try {
-                const systemctlStatusBloss = require('./ssh/systemctl-status-bloss');
-                const sshExecutor = require('./sshexecutor')(server = systemctlStatusBloss.server, systemctlStatusBloss.commands, systemctlStatusBloss.msg, systemctlStatusBloss.debug, 'statusChannel');
-                var sshparams = sshExecutor.getSshExecutor();
-                sshparams.commands = [cmd];
-                sshparams.onEnd = function (sessionText, sshparams) {
-                    logger.info(JSON.stringify(sessionText));
-                    if (service != null) {
-                        getServiceStatus(service);
-                    }
-                };
-                var SSH = new ssh2shell(sshparams);
-                SSH.on('end', function (sessionText, sshparams) {
-                    this.emit('msg', sessionText);
-                })
-                SSH.connect();
-            } catch (error) {
-                logger.error(error);
-            }
-        }
-
-
-        // Function to retrieve the serviceName status on the controller configured in .env
-        function getServiceStatus(serviceName) {
-            try {
-                const systemCtlIsActive = require('./ssh/systemctl-is-active');
-                command = ["sudo systemctl is-active " + serviceName];
-                const sshExecutor = require('./sshexecutor')(server = systemCtlIsActive.server, commands = command, msg = systemCtlIsActive.msg, debug = systemCtlIsActive.debug, websocketChannel = 'statusChannel');
-                var sshParamsForStatusRetrieval = sshExecutor.getSshExecutor();
-                sshParamsForStatusRetrieval.onEnd = function (sessionTextt) {
-                    if (sessionTextt.includes("inactive")) {
-                        logger.info(serviceName + " is inactive");
-                        socket.emit('statusChannel', { [serviceName]: "inactive" });
-                    } else if (sessionTextt.includes("active")) {
-                        logger.info(serviceName + " is active");
-                        socket.emit('statusChannel', { [serviceName]: "active" });
-                    }
-                };
-                var SSH = new ssh2shell(sshParamsForStatusRetrieval);
-                SSH.on('end', function (sessionTextt) {
-                    this.emit('msg', sessionTextt);
-                    logger.info('we here');
-                })
-                SSH.connect();
-
-
-            } catch (error) {
-                logger.error(error);
-            }
-        }
+            // }
+        });    
     });
-
-    return io;
 };
--- server.js
+++ server.js
@@ -4,7 +4,7 @@
 * Module dependencies.
 */
 const express = require('express');
-const logger = require('./modules/logger');
+//const logger = require('./modules/console');
 const http = require('http');
 const socketio = require('socket.io');
 const dotenv = require('dotenv');
@@ -13,12 +13,14 @@ const bodyParser = require('body-parser')
 const mongoose = require('mongoose');
 const chalk = require('chalk');
 
+const socketHandler = require("./modules/sockets");
+
 
 /**
  * Start message
  */
-logger.info('Starting BloSS collector')
-logger.info('Listening on PORT: ' + process.env.WS_PORT);
+console.info('Starting BloSS collector')
+console.info('Listening on PORT: ' + process.env.WS_PORT);
 
 /**
  * Load environment variables from .env file
@@ -31,94 +33,23 @@ dotenv.load({ path: '.env' });
 const app = express();
 // app.use(express.json()); // Add JSON middleware
 //app.use(express.json({strict: true})); // Add JSON middleware
-app.use(bodyParser.json({ strict: false }));
+//app.use(bodyParser.json({ strict: false }));
 const server = http.Server(app);
 server.listen(process.env.WS_PORT);
-var io = require("./modules/sockets")(server);
+var io = require("socket.io")(server);
 
+io.on('connection', function (socket) {
+    console.info("connection established")
 
-/**
- * Connect MongoDB with mongoose
- */
-mongoose.connect(process.env.MONGO_DB, { useNewUrlParser: true })
-    .then(() => logger.info('Connected to MongoDB'))
-    .catch(err => logger.error('Could not connect to MongoDB', err));
+    socket.on('isControllerAvailableRequest', function(data){
+        console.info("isControllerAvailable called");
+    });
 
-// Define report schema
-const reportSchema = new mongoose.Schema({
-    hash: String,
-    target: String,
-    timestamp: { type: Date, default: Date.now },
-    created: { type: Date, default: Date.now },
-    action: String,
-    subnetwork: String,
-    addresses: [String]
-});
-const Report = mongoose.model('Report', reportSchema);
+    socket.on('statusPolling', function(data){
+        console.info("status polling");
+    });
 
-
-/** 
- * Define REST API for interaction with bloss-core
- */
-app.post('/api/v1.0/report', (req, res) => {
-
-    // This check is necessary for testing with Postman, data already arrives as JS Object and doesn't need parsing
-    var attack_report;
-    try {
-        attack_report = JSON.parse(req.body);
-    } catch (e) {
-        attack_report = req.body;
-        // logger.info("NOT JSON");
-    }
-
-    var bgHex = getRandomColor();
-    var hex = getColorByBgColor(bgHex);
-    logger.info("New Report with hash " + chalk.hex(hex).bgHex(bgHex).bold([attack_report.hash]) + " posted.")
-
-    function getColorByBgColor(bgColor) {
-        if (!bgColor) { return ''; }
-        return (parseInt(bgColor.replace('#', ''), 16) > 0xffffff / 2) ? '#000' : '#fff';
-    }
-
-    function getRandomColor() {
-        var letters = '0123456789ABCDEF';
-        var color = '#';
-        for (var i = 0; i < 6; i++) {
-            color += letters[Math.floor(Math.random() * 16)];
-        }
-        return color;
-    }
-
-    // Timestamps sent from Bloss are not correctly formatted (2018-10-30-07:41:09 instead of 2018-10-30T07:41:09)
-    var bodyTimeStampClean = replaceAt(attack_report.timestamp, 10, "T")
-    function replaceAt(substring, index, replacement) {
-        if (substring != null) {
-            return substring.substr(0, index) + replacement + substring.substr(index + replacement.length);
-        }
-        else return null;
-    }
-    var timestamp = new Date(Date.parse(bodyTimeStampClean)); // UTC
-    //console.log(timestamp);
-
-    // This should only be called when the hash of the new report HAS NOT YET BEEN SAVED.
-    createReport();
-
-    async function createReport() {
-        const report = new Report({
-            hash: attack_report.hash,
-            target: attack_report.target,
-            timestamp: timestamp,
-            action: attack_report.action,
-            subnetwork: attack_report.subnetwork,
-            addresses: attack_report.addresses
-        })
-
-        const result = await report.save();
-        // console.log("We here" + result);
-        logger.info("New Report with hash " + chalk.hex(hex).bgHex(bgHex).bold([result.hash]) + " persisted and relayed.")
-        // logger.info("New Report with hash "+ chalk.bgGreen.black.bold([result.hash]) +" saved and relayed.")
-        io.emit('reportChannel', { data: result });
-    }
-
-    res.json({ message: 'Report delivered' });
+    socket.on('serviceCtl', function (data) {
+        console.info("serviceCtl called");
+    });    
 });
